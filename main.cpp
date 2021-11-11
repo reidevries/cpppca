fff8f441e34f5d871851fb50b7d8bb612033a51a
rei de vries <reidv@protonmail.com>
2021-11-11 19:18:10 +1030

    convert PCA to class

--- main.cpp
+++ main.cpp
@@ -15,6 +15,44 @@ auto covariance( Matrix<Scalar, rows, cols> X ) -> Matrix<Scalar, cols, cols>
 	return X_centered.transpose() * X_centered;
 }
 
+template<typename Scalar, u64 t_rows, u64 t_cols>
+class PrincipleComponentAnalysis
+{
+	Matrix<Scalar, t_rows, t_cols> X;
+	Matrix<Scalar, t_cols, t_cols> C;
+	SelfAdjointEigenSolver<Matrix<Scalar, t_cols, t_cols>> solver;
+
+public:
+	PrincipleComponentAnalysis(Matrix<Scalar, t_rows, t_cols> _X)
+		: X(_X),
+		  C(covariance<Scalar, t_rows, t_cols>(_X)),
+		  solver(C)
+	{
+		if (solver.info() != Success) abort();
+	}
+
+	template<u64 k>
+	auto projection_matrix() -> Matrix<Scalar, t_cols, k>
+	{
+		// wtf?? why do i have to write with  this weird syntax??
+		return solver.eigenvectors().template block<t_cols,k>(0,0);
+	}
+
+	template<u64 k>
+	auto project() -> Matrix<Scalar, t_rows, k>
+	{
+		auto P = projection_matrix<k>();
+		return X * P;
+	}
+
+	template<u64 k>
+	auto reconstruct(Matrix<Scalar, t_rows, k> Z) -> Matrix<Scalar, t_rows, t_cols>
+	{
+		auto P = projection_matrix<k>();
+		return Z * P.adjoint();
+	}
+};
+
 int main()
 {
 	static constexpr int ROWS = 5;
@@ -27,18 +65,10 @@ int main()
 		{11, 3, 2, 1, 7,11},
 		{ 5,23,10, 4, 3, 6}
 	};
-	auto out = covariance<SCALAR_TYPE,ROWS,COLS>(X);
-	std::cout << out << std::endl;
-	auto eigensolver
-		= SelfAdjointEigenSolver<Matrix<SCALAR_TYPE,COLS,COLS>>(out);
-	if (eigensolver.info() != Success) abort();
-	std::cout << "The eigenvalues of A are:\n" << eigensolver.eigenvalues() << std::endl;
-	std::cout << "Here's a matrix whose columns are eigenvectors of A \n"
-			<< "corresponding to these eigenvalues:\n"
-			<< eigensolver.eigenvectors() << std::endl;
-	auto P = eigensolver.eigenvectors().block<COLS,COLS_REDUCED>(0,0);
-	auto Z = X*P;
-	auto X_hat = Z*P.adjoint();
-	std::cout << X_hat << std::endl;
 
+	auto pca = PrincipleComponentAnalysis<SCALAR_TYPE, ROWS, COLS>(X);
+	auto Z = pca.project<5>();
+	std::cout << Z << std::endl;
+	auto X_hat = pca.reconstruct<5>(Z);
+	std::cout << X_hat << std::endl;
 }
